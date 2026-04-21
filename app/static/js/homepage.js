b277f7503baf22383ee86314e41d04acae545779
jashil8r <jashilramchurn@gmail.com>
2026-04-21 18:12:08 +0800

    added switchtab js function that changes signup tab to login tab and implemented it in the html

--- app/static/js/homepage.js
+++ app/static/js/homepage.js
@@ -5,3 +5,22 @@ function handleAuth(type) {
         console.log('logging in...');
     }
 }
+
+function switchTab(tab) {
+    const suForm = document.getElementById('formsu');
+    const liForm = document.getElementById('formli');
+    const tabSu = document.getElementById('tabsu');
+    const tabLi = document.getElementById('tabli');
+
+    if (tab === 'su') {
+        suForm.classList.remove('hidden');
+        liForm.classList.add('hidden');
+        tabSu.classList.add('active');
+        tabLi.classList.remove('active');
+    } else {
+        liForm.classList.remove('hidden');
+        suForm.classList.add('hidden');
+        tabLi.classList.add('active');
+        tabSu.classList.remove('active');
+    }
+}
