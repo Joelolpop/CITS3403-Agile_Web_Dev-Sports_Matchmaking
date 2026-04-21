227ffc7fb39f0741ebe576201d571388c319a6e5
TheBabieGamer <thisenkmalalasena@gmail.com>
2026-04-21 18:55:00 +0800

    Updated routes to include the matching page

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ main = Blueprint('main', __name__)
 @main.route("/")
 def base():
     return render_template("base.html")
+
+@main.route('/matching')
+def matching():
+    return render_template('matching.html')
