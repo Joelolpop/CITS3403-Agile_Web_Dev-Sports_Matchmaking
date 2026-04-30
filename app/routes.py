cb0a851e9d7ccabec5d9f5d6e685f67f1e927732
jashil8r <jashilramchurn@gmail.com>
2026-04-30 22:28:01 +0800

    added sav,delete,cancel buttton,cancel confirmation and configured route for event_edit

--- app/routes.py
+++ app/routes.py
@@ -20,38 +20,8 @@ def profile():
 
 @main.route("/events/<int:event_id>")
 def event_view(event_id):
-    event = {
-        "id": 1,
-        "name": "Tennis at UWA",
-        "sport": "Tennis",
-        "host": "alexsmith",
-        "date": "Sat 3 May 2026",
-        "time": "10:00 AM",
-        "location": "Nedlands",
-        "postcode": 6009,
-        "description": "Casual doubles at the UWA courts. All skill levels welcome. Bring your own racket if you have one, balls provided.",
-        "spots_total": 10,
-        "spots_filled": 3,
-        "attendees": [
-            {"name": "alexsmith", "is_host": True},
-            {"name": "jake_p", "is_host": False},
-            {"name": "Sara92", "is_host": False},
-        ]
-        
-    }
-    return render_template("event_view.html", event=event, user_has_joined=False)
+    return render_template("event_view.html", event_id=event_id,user_has_joined=False)
 
 @main.route("/events/<int:event_id>/edit")
 def event_edit(event_id):
-    event = {
-        "id": 1,
-        "name": "Tennis at UWA",
-        "sport": "Tennis",
-        "date": "2026-05-03",
-        "time": "10:00",
-        "location": "Nedlands",
-        "postcode": 6009,
-        "description": "Casual doubles at the UWA courts. All skill levels welcome. Bring your own racket if you have one, balls provided.",
-        "spots_total": 10,
-    }
-    return render_template("event_edit.html", event=event)
+    return render_template("event_edit.html", event_id=event_id)
