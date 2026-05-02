a30d8375540e73ace08c938e015eb8a0b3e17576
Narendra Madiadipoera <narendra.madiadipoera@gmail.com>
2026-05-02 22:42:55 +0800

    Implementing a "Split-Screen" design where the user is able to browse events that has been joined and events that are available to them.

--- app/routes.py
+++ app/routes.py
@@ -20,6 +20,15 @@ def friend_data():
     return render_template("friend_data_view.html")
 
 
+@main.route("/events")
+def events_joined_available():
+    return render_template(
+        "event_joined_available.html")
+
+
+@main.route("/events/joined-available")
+def events_joined_available_alias():
+    return render_template("event_joined_available.html")
 
 
 
