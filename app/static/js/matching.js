f4ab2062720cc781367365f4569851b660f6a4b9
TheBabieGamer <thisenkmalalasena@gmail.com>
2026-05-06 01:56:01 +0800

    Updated js for matching to have a transition when button is clicked

--- app/static/js/matching.js
+++ app/static/js/matching.js
@@ -1,6 +1,17 @@
-function removeCard(event) {
-    const col = event.target.closest('.col-12, .col-md-6, .col-lg-4');
-    if (col) {
-        col.remove();
+function handleConnect(button) {
+    const cardContainer = button.closest('.player-card-container');
+    
+    if (cardContainer) {
+
+        button.disabled = true;
+        button.innerHTML = "Request Sent!";
+        button.classList.replace('btn-success', 'btn-outline-secondary');
+
+        cardContainer.style.transition = 'all 0.5s ease';
+        cardContainer.style.opacity = '0';
+
+        setTimeout(() => {
+            cardContainer.remove();
+        }, 500);
     }
 }
