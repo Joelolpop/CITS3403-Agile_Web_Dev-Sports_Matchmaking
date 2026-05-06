0c9106507065b02b35284ec4d999253a6d7e184e
Narendra Madiadipoera <narendra.madiadipoera@gmail.com>
2026-05-06 22:24:49 +0800

    Implementing Matching table with boolean constraints

--- app/models.py
+++ app/models.py
@@ -15,3 +15,21 @@ class Users(db.Model):
     sport_1    = db.Column(db.String(64), nullable=False)
     sport_2    = db.Column(db.String(64), nullable=False)
     sport_3    = db.Column(db.String(64), nullable=False)
+
+class Matching(db.Model):
+    __tablename__ = 'matching'
+
+    matching_id = db.Column(db.Integer, primary_key=True)
+    user_a_id   = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    user_b_id   = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    response_a  = db.Column(db.String(8))   # 'Accept' or 'Reject'
+    response_b  = db.Column(db.String(8))   # 'Accept' or 'Reject'
+
+    user_a = db.relationship('Users', foreign_keys=[user_a_id])
+    user_b = db.relationship('Users', foreign_keys=[user_b_id])
+
+    @property
+    def result(self):
+        if self.response_a == 'Accept' and self.response_b == 'Accept':
+            return 'Friends'
+        return 'Skip'
