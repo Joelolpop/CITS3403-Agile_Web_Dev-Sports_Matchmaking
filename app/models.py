c3334ed40c5240b4eae95628e80c877124b70d05
Narendra Madiadipoera <narendra.madiadipoera@gmail.com>
2026-05-07 16:04:26 +0800

    updated the models.py to supports POST and GET request

--- app/models.py
+++ app/models.py
@@ -1,20 +1,34 @@
 from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 SPORTS = ["Soccer", "Basketball", "Tennis", "Netball", "Cricket", "Australian Rules Football", "Swimming", "Rugby", "Golf"]
 GENDER = ["M","F","Others"]
 RESPONSE_A = ["Accept", "Reject"]
 RESPONSE_B = ["Accept", "Reject"]
 
-class Users(db.Model):
+class Users(UserMixin, db.Model):
     __tablename__ = 'users'
-    user_id    = db.Column(db.Integer, primary_key=True)
-    username   = db.Column(db.String(64), unique=True, nullable=False)
-    first_name = db.Column(db.String(64), nullable=False)
-    last_name  = db.Column(db.String(64), nullable=False)
-    gender     = db.Column(db.String(16), nullable=False)
-    postcode   = db.Column(db.String(10), nullable=False)
-    sport_1    = db.Column(db.String(64), nullable=False)
-    sport_2    = db.Column(db.String(64), nullable=False)
-    sport_3    = db.Column(db.String(64), nullable=False)
+    user_id       = db.Column(db.Integer, primary_key=True)
+    username      = db.Column(db.String(64), unique=True, nullable=True)
+    email         = db.Column(db.String(120), unique=True, nullable=False)
+    first_name    = db.Column(db.String(64), nullable=False)
+    last_name     = db.Column(db.String(64), nullable=False)
+    gender        = db.Column(db.String(16))
+    postcode      = db.Column(db.String(10))
+    sport_1       = db.Column(db.String(64))
+    sport_2       = db.Column(db.String(64))
+    sport_3       = db.Column(db.String(64))
+    password_hash = db.Column(db.String(256), nullable=False)
+
+    def get_id(self):
+        return str(self.user_id)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Matching(db.Model):
     __tablename__ = 'matching'
