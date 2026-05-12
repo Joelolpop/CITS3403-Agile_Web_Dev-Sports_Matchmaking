baccb3eb400f9430fc7c24c7fecf46978bd533c7
Narendra Madiadipoera <narendra.madiadipoera@gmail.com>
2026-05-12 15:41:39 +0800

    Changing the login to use wtforms validators

--- app/forms.py
+++ app/forms.py
@@ -1,25 +1,9 @@
-from flask import request
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, PasswordField, StringField
+from wtforms.validators import DataRequired, Email
 
 
-class _Field:
-    def __init__(self, data):
-        self.data = data
-
-
-class LoginForm:
-    """Minimal form object that mirrors Flask-WTF's login usage pattern."""
-
-    def __init__(self):
-        email = request.form.get("email", "").strip().lower()
-        password = request.form.get("password", "")
-        remember_raw = request.form.get("remember_me", "")
-        remember = str(remember_raw).lower() in {"1", "true", "on", "yes"}
-
-        self.email = _Field(email)
-        self.password = _Field(password)
-        self.remember_me = _Field(remember)
-
-    def validate_on_submit(self):
-        if request.method != "POST":
-            return False
-        return bool(self.email.data and self.password.data)
+class LoginForm(FlaskForm):
+    email = StringField(validators=[DataRequired(), Email()])
+    password = PasswordField(validators=[DataRequired()])
+    remember_me = BooleanField(default=False)
