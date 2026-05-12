f5a15715cca990a3ae9484254352cec645599cef
Narendra Madiadipoera <narendra.madiadipoera@gmail.com>
2026-05-12 16:41:15 +0800

    ensuring CSRF tokens are properly registerred in friends_data

--- app/static/js/friends_data_view.js
+++ app/static/js/friends_data_view.js
@@ -1,6 +1,8 @@
 document.getElementById('removeFriendBtn').addEventListener('click', function() {
     const friendId = this.getAttribute('data-friend-id');
     const redirectUrl = this.getAttribute('data-friend-url');
+    const csrfTokenMeta = document.querySelector('meta[name="csrf-token"]');
+    const csrfToken = csrfTokenMeta ? csrfTokenMeta.getAttribute('content') : null;
     
     if (confirm('Are you sure you want to remove this friend?')) {
 
@@ -9,7 +11,8 @@ document.getElementById('removeFriendBtn').addEventListener('click', function() 
         fetch(`/friends/${friendId}/remove`, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken || ''
             }
         })
         .then(() => {
