2c0b0ad4cc991c53520e7385a99cf83a92460568
TheBabieGamer <thisenkmalalasena@gmail.com>
2026-05-14 17:42:09 +0800

    Implemented test class for Match Scores

--- tests/test_models.py
+++ tests/test_models.py
@@ -17,7 +17,7 @@ def add_test_data():
         username="testuser2",
         first_name="Isaac", last_name="Newton",
         email="isaac@gmail.com", postcode=6001,
-        sport_1="Tennis", sport_2="Basketball"
+        sport_1="Swimming", sport_2="Basketball"
     )
     user2.set_password("password2")
 
@@ -62,5 +62,52 @@ class UserModelTestCase(unittest.TestCase):
         user = Users.query.filter_by(email="albert@gmail.com").first()
         self.assertIsInstance(user.get_id(), str)
 
+class MatchingScoreTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        add_test_data()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_match_score_one_common_sport(self):
+        user1 = Users.query.filter_by(email="albert@gmail.com").first()
+        user2 = Users.query.filter_by(email="isaac@gmail.com").first()
+
+        #Albert and Isaac have 1 common sport (Basketball) and are 1 postcode apart
+        score = calculate_match_score(user1, user2)
+        self.assertEqual(score, 3)
+    
+    def test_match_score_no_common_sport(self):
+        user1 = Users.query.filter_by(email="albert@gmail.com").first()
+        user3 = Users.query.filter_by(email="jason@gmail.com").first()
+
+        #Albert and Jason have no common sports and are 10 postcodes apart
+        score = calculate_match_score(user1, user3)
+        self.assertEqual(score, 13)
+    
+    def test_match_score_no_postcode(self):
+        user1 = Users.query.filter_by(email="albert@gmail.com").first()
+
+        user_no_postcode = Users(
+            username="nopostcode",
+            first_name="No", last_name="Postcode",
+            email="nopost@gmail.com", sport_1 = "Tennis"
+        )
+        user_no_postcode.set_password("nopassword")
+        db.session.add(user_no_postcode)
+        db.session.commit()
+
+        score = calculate_match_score(user1, user_no_postcode)
+
+        #check if missing postcode is handled correctly
+
+        self.assertIsNotNone(score)
 
 
